3b60fe4206924917ea1cb10f3313b06dd054bcf4
Hyxda <hyxder@icloud.com>
2023-02-07 10:34:56 -0500

    role1.py updated
    Added battle attributes for mage. Defined function for mage which will be used later for character selection.

--- role1.py
+++ role1.py
@@ -1,1 +1,11 @@
+# Define battle attributes for Role1: Mage. Mage excels in magic but lacks stealth.
+mage_health = 3
+mage_magic = 3
+mage_stealth = 1
+mage_intelligence = 2
 
+def mage():
+    """
+    This function returns the string "mage" & is used for character selection.
+    """
+    return "mage"
