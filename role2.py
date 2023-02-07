c939efcd0ef4b4b31aefbbad860d8130a13a87ad
Hyxda <hyxder@icloud.com>
2023-02-07 10:38:18 -0500

    role2.py updated
    Added battle attributes for ninja. Defined function for ninja which will be used later for character selection.

--- role2.py
+++ role2.py
@@ -1,1 +1,11 @@
+# Define battle attributes for Role2: NInja. Ninja excels in stealth but lacks magic.
+ninja_health = 3
+ninja_magic = 1
+ninja_stealth = 3
+ninja_intelligence = 2
 
+def ninja():
+    """
+    This function returns the string "ninja" & is used for character selection.
+    """
+    return "ninja"
